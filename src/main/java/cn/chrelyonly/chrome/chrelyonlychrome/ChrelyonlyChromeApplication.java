a2c173637c7905a7b3bb6f5053fb95e9cc0dc3f6
chrelyonly <1172576293@qq.com>
2025-03-12 16:50:15 +0800

    1

--- src/main/java/cn/chrelyonly/chrome/chrelyonlychrome/ChrelyonlyChromeApplication.java
+++ src/main/java/cn/chrelyonly/chrome/chrelyonlychrome/ChrelyonlyChromeApplication.java
@@ -1,8 +1,6 @@
 package cn.chrelyonly.chrome.chrelyonlychrome;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -22,7 +20,7 @@ public class ChrelyonlyChromeApplication {
 //		options.setBinary("/app/chrome/chrome"); // 设置 Chrome 可执行文件路径
 		options.addArguments("--headless"); // 无头模式
 		options.addArguments("--window-size=1920,1080"); // 设置窗口大小
-
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36");
 		// 设置远程 WebDriver 连接地址
 //		URL remoteUrl = new URL("http://192.168.10.91:34444");
 		URL remoteUrl = new URL("https://chrome-api.frp.chrelyonly.cn");
@@ -30,10 +28,12 @@ public class ChrelyonlyChromeApplication {
 		WebDriver driver = new RemoteWebDriver(remoteUrl,options);
 		try {
 			// 打开目标网页
-			driver.get("https://www.baidu.com");
-
+			driver.get("https://act.7881.com/pc/goldcoin/index.html?gameId=G10");
+			Thread.sleep(3000L);
+//			寻找指定区域元素
+			WebElement driverElement = driver.findElement(By.className("type01"));
 			// 截取网页截图并保存为文件
-			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File screenshot = driverElement.getScreenshotAs(OutputType.FILE);
 
 			// 定义保存路径保存在当前目录
 			File destination = new File(System.currentTimeMillis() + ".png");
@@ -41,11 +41,11 @@ public class ChrelyonlyChromeApplication {
 			// 保存截图文件到指定路径
 			Files.copy(screenshot.toPath(), destination.toPath());
 
-			System.out.println("Screenshot saved successfully at: " + destination.getAbsolutePath());
+			System.out.println("快照保存成功: " + destination.getAbsolutePath());
 
-		} catch (IOException e) {
-			System.out.println("Failed to save screenshot: " + e.getMessage());
-		} finally {
+		} catch (Exception e) {
+			System.out.println("快照保存失败: " + e.getMessage());
+        } finally {
 			// 关闭浏览器,一定要执行
 			driver.quit();
 		}
