6bbb7c6fdd9a0a9f872481f5167118454a683553
chrelyonly <1172576293@qq.com>
2025-03-13 19:16:02 +0800

    1

---
+++ Dockerfile-system
@@ -0,0 +1,14 @@
+# 使用最小化的 OpenJDK 运行时环境
+FROM selenium/standalone-chrome
+
+#设置工作目录
+#WORKDIR /app
+
+# 将应用的 JAR 文件添加到容器中
+#ADD target/chrelyonly-chrome-0.0.1-SNAPSHOT.jar /app/app.jar
+#ADD chromedriver /app/chrome/chromedriver
+#ADD chrome-linux64 /app/chrome/
+#给予权限
+# 定义容器启动时运行的命令
+#CMD ["java", "-jar", "app.jar"]
+#CMD ["sh"]
--- src/main/java/cn/chrelyonly/chrome/chrelyonlychrome/ChrelyonlyChromeApplication.java
+++ src/main/java/cn/chrelyonly/chrome/chrelyonlychrome/ChrelyonlyChromeApplication.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class ChrelyonlyChromeApplication {
+
 
 	public static void main(String[] args) throws MalformedURLException {
 		// 设置 ChromeDriver 路径（根据你的实际情况设置）
@@ -22,8 +26,8 @@ public class ChrelyonlyChromeApplication {
 		options.addArguments("--window-size=1920,1080"); // 设置窗口大小
 		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36");
 		// 设置远程 WebDriver 连接地址
-//		URL remoteUrl = new URL("http://192.168.10.91:34444");
-		URL remoteUrl = new URL("https://chrome-api.frp.chrelyonly.cn");
+		URL remoteUrl = new URL("http://172.16.3.167:34444");
+//		URL remoteUrl = new URL("https://chrome-api.frp.chrelyonly.cn");
 		// 创建 WebDriver 实例
 		WebDriver driver = new RemoteWebDriver(remoteUrl,options);
 		try {
@@ -33,15 +37,15 @@ public class ChrelyonlyChromeApplication {
 //			寻找指定区域元素
 			WebElement driverElement = driver.findElement(By.className("type01"));
 			// 截取网页截图并保存为文件
-			File screenshot = driverElement.getScreenshotAs(OutputType.FILE);
+			String screenshot = driverElement.getScreenshotAs(OutputType.BASE64);
 
 			// 定义保存路径保存在当前目录
-			File destination = new File(System.currentTimeMillis() + ".png");
+//			File destination = new File(System.currentTimeMillis() + ".png");
 
 			// 保存截图文件到指定路径
-			Files.copy(screenshot.toPath(), destination.toPath());
+//			Files.copy(screenshot.toPath(), destination.toPath());
 
-			System.out.println("快照保存成功: " + destination.getAbsolutePath());
+			System.out.println("快照保存成功: data:image/png;base64," + screenshot);
 
 		} catch (Exception e) {
 			System.out.println("快照保存失败: " + e.getMessage());
