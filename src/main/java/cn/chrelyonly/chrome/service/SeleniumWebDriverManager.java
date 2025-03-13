5b5e3a2f50a7f72bdd71483581aece9c6f509ce5
chrelyonly <1172576293@qq.com>
2025-03-13 20:08:40 +0800

    1

--- src/main/java/cn/chrelyonly/chrome/service/SeleniumWebDriverManager.java
+++ src/main/java/cn/chrelyonly/chrome/service/SeleniumWebDriverManager.java
@@ -2,10 +2,7 @@ package cn.chrelyonly.chrome.service;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.stereotype.Component;
@@ -20,16 +17,15 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class SeleniumWebDriverManager {
-    private final WebDriver driver;
+    private final ChromeOptions options;
+    private final URL remoteUrl = new URL("http://172.18.0.5:4444");
+//    private final URL remoteUrl = new URL("http://172.16.3.167:34444");
 
     public SeleniumWebDriverManager() throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
+        options = new ChromeOptions();
         options.addArguments("--headless", "--window-size=1920,1080");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36");
 // 远程 WebDriver 服务器
-//        URL remoteUrl = new URL("http://172.16.3.167:34444");
-        URL remoteUrl = new URL("http://172.18.0.5:4444");
-        this.driver = new RemoteWebDriver(remoteUrl, options);
         System.out.println("WebDriver 初始化完成！");
     }
 
@@ -37,22 +33,19 @@ public class SeleniumWebDriverManager {
      * 获取dnf金币比例快照
      */
     public String getDnfScreenshot() {
+        WebDriver driver = new RemoteWebDriver(remoteUrl, options);
         driver.get("https://act.7881.com/pc/goldcoin/index.html?gameId=G10");
         try {
             // 等待页面加载
             Thread.sleep(3000);
             WebElement driverElement = driver.findElement(By.className("type01"));
+//            return "data:image/png;base64," + driverElement.getScreenshotAs(OutputType.BASE64);
             return "data:image/png;base64," + driverElement.getScreenshotAs(OutputType.BASE64);
         } catch (Exception e) {
             e.printStackTrace();
             return "截图失败：" + e.getMessage();
-        }
-    }
-    @PreDestroy
-    public void cleanup() {
-        if (this.driver != null) {
-            this.driver.quit();
-            System.out.println("WebDriver 已关闭！");
+        }finally {
+            driver.quit();
         }
     }
 }
