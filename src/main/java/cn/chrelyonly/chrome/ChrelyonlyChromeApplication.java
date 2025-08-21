22cd4e3c69ff393eca8324d7b3e5f2d2a41b715f
chrelyonly <1172576293@qq.com>
2025-08-21 10:55:29 +0800

    1

--- src/main/java/cn/chrelyonly/chrome/ChrelyonlyChromeApplication.java
+++ src/main/java/cn/chrelyonly/chrome/ChrelyonlyChromeApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author 11725
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @Slf4j
 @EnableAsync
+@EnableScheduling
 public class ChrelyonlyChromeApplication {
 
 	public static void main(String[] args) {
