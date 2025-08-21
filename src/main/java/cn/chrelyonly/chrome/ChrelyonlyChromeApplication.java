9d1d2be7037dfad8e13d72ed5aa3947aa09cf7a4
chrelyonly <1172576293@qq.com>
2025-08-21 12:34:12 +0800

    Revert "1"
    This reverts commit 22cd4e3c69ff393eca8324d7b3e5f2d2a41b715f.

--- src/main/java/cn/chrelyonly/chrome/ChrelyonlyChromeApplication.java
+++ src/main/java/cn/chrelyonly/chrome/ChrelyonlyChromeApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author 11725
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @Slf4j
 @EnableAsync
-@EnableScheduling
 public class ChrelyonlyChromeApplication {
 
 	public static void main(String[] args) {
